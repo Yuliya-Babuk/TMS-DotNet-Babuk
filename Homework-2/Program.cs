36b7ba53522bade10bfb962f04f4dc26f05b1db6
Julia Babuk <babukjulia1@gmail.com>
2020-12-10 14:30:35 +0300

    init Datehandler

---
+++ Homework-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите дату и узнайте день недели!");
+
+            var Date = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine(Date.DayOfWeek);
+
+        }
+    }
+}
